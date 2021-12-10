6712fef240e3d5fd0d3bc7415c921bd8a8a9ec24
Radoslav Bekyarov <rbekyarov82@gmail.com>
2021-12-10 14:20:21 +0200

    Matrices Lab

---
+++ src/ADV03MultidimensionalArrays/p5MaximumSumOf2x2Submatrix.java
@@ -0,0 +1,42 @@
+package ADV03MultidimensionalArrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class p5MaximumSumOf2x2Submatrix {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] inputRowColumns = Arrays.stream(scanner.nextLine().split(", ")).mapToInt(Integer::parseInt).toArray();
+        int rows = inputRowColumns[0];
+        int columns = inputRowColumns[1];
+        int[][] matrix = new int[rows][columns];
+        for (int i = 0; i < matrix.length; i++) {
+            int[] inputData = Arrays.stream(scanner.nextLine().split(", ")).mapToInt(Integer::parseInt).toArray();
+            for (int j = 0; j < matrix[0].length; j++) {
+                matrix[i][j] = inputData[j];
+            }
+        }
+        int maxValue = Integer.MIN_VALUE;
+        int[][] maxSumMatrix = new int[2][2];
+        for (int row = 0; row < rows - 1; row++) {
+            for (int column = 0; column < columns - 1; column++) {
+                int sum = matrix[row][column] + matrix[row][column + 1] +
+                        matrix[row + 1][column] + matrix[row + 1][column + 1];
+                if (sum > maxValue) {
+                    maxValue = sum;
+                    maxSumMatrix[0][0] = matrix[row][column];
+                    maxSumMatrix[0][1] = matrix[row][column + 1];
+                    maxSumMatrix[1][0] = matrix[row + 1][column];
+                    maxSumMatrix[1][1] = matrix[row + 1][column + 1];
+                }
+            }
+        }
+        for (int i = 0; i < maxSumMatrix.length; i++) {
+            for (int j = 0; j < maxSumMatrix[1].length; j++) {
+                System.out.print(maxSumMatrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println(maxValue);
+    }
+}
