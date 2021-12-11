348b513e58105d9c8d5f91b22fb7280c25190a81
Radoslav Bekyarov <rbekyarov82@gmail.com>
2021-12-11 17:09:55 +0200

    Exercise Matrix

---
+++ src/ADV04MultidimensionalArraysExercise/p2Matrix_Of_Palindromes.java
@@ -0,0 +1,27 @@
+package ADV04MultidimensionalArraysExercise;
+
+import java.util.Scanner;
+
+public class p2Matrix_Of_Palindromes {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int rows = scanner.nextInt();
+        int cols = scanner.nextInt();
+        String[][] matrix = new String[rows][cols];
+        char startletter = 'a';
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                String palindrome = "" + startletter + (char) (startletter + col) + startletter;
+                matrix[row][col] = palindrome;
+            }
+            startletter = (char) (startletter + 1);
+        }
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
