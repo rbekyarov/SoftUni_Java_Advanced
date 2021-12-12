6e3551544275efb4173cce5c25d57da24bff6597
Radoslav Bekyarov <rbekyarov82@gmail.com>
2021-12-12 12:58:11 +0200

    Set and Maps

---
+++ src/ADV05SetsAndMapsAdvanced/p3CountRealNumbers.java
@@ -0,0 +1,28 @@
+package ADV05SetsAndMapsAdvanced;
+
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class p3CountRealNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double[] input = Arrays.stream(scanner.nextLine().split(" "))
+                .mapToDouble(Double::parseDouble).toArray();
+        Map<Double, Integer> map = new LinkedHashMap<>();
+
+        for (Double e : input) {
+            if (!map.containsKey(e)) {
+                map.put(e, 1);
+            } else {
+                int valueMap = map.get(e);
+                map.put(e, ++valueMap);
+            }
+        }
+        for (Map.Entry<Double, Integer> entry : map.entrySet()) {
+            System.out.printf("%.1f -> %d%n", entry.getKey(), entry.getValue());
+        }
+    }
+}
