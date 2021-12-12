66d65482e42d92f9381e08a1eaa3aee5738c0c8d
Radoslav Bekyarov <rbekyarov82@gmail.com>
2021-12-12 10:07:20 +0200

    Set and Maps

---
+++ src/ADV05SetsAndMapsAdvanced/p1ParkingLot.java
@@ -0,0 +1,27 @@
+package ADV05SetsAndMapsAdvanced;
+
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+
+
+public class p1ParkingLot {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] input = scanner.nextLine().split(", ");
+        LinkedHashSet<String> parkingLot = new LinkedHashSet<>();
+
+        while (!input[0].equals("END")) {
+            String car = input[1];
+
+            if (input[0].equals("IN")) {
+                parkingLot.add(car);
+            } else {
+                parkingLot.remove(car);
+            }
+            input = scanner.nextLine().split(", ");
+        }
+        for (String car : parkingLot) {
+            System.out.println(car);
+        }
+    }
+}
