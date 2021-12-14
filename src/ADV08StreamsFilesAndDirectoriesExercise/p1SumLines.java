e7ac2fdba808fda4c4b18e65a3cd6bf3422aa2ed
Radoslav Bekyarov <rbekyarov82@gmail.com>
2021-12-14 11:36:16 +0200

    Streams Files and Directories

---
+++ src/ADV08StreamsFilesAndDirectoriesExercise/p1SumLines.java
@@ -0,0 +1,29 @@
+package ADV08StreamsFilesAndDirectoriesExercise;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+public class p1SumLines {
+    public static void main(String[] args) {
+        String path = "E:\\folder\\Java-Advanced-Files-and-Streams-Exercises-Resources\\input.txt";
+
+        List<String> inputList = null;
+        try {
+            inputList = Files.readAllLines(Path.of(path));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        for (String line : inputList) {
+            int sumCharLine = 0;
+            for (int i = 0; i <line.length() ; i++) {
+                char currentChar = line.charAt(i);
+                sumCharLine+=currentChar;
+            }
+            System.out.println(sumCharLine);
+
+        }
+    }
+}
