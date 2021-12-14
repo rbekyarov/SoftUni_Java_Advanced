90cd24472a560f8569c2a2f3897b65d68a55bb7a
Radoslav Bekyarov <rbekyarov82@gmail.com>
2021-12-14 11:50:04 +0200

    Streams Files and Directories

---
+++ src/ADV08StreamsFilesAndDirectoriesExercise/p2SumBytes.java
@@ -0,0 +1,21 @@
+package ADV08StreamsFilesAndDirectoriesExercise;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class p2SumBytes {
+    public static void main(String[] args) throws IOException {
+        String path = "E:\\folder\\Java-Advanced-Files-and-Streams-Exercises-Resources\\input.txt";
+        FileInputStream fileStream = new FileInputStream(path);
+        Scanner scanner = new Scanner(fileStream);
+        int sumAllSymbols = 0;
+        while (scanner.hasNext()){
+            String text = scanner.nextLine();
+            for (char symbol : text.toCharArray()){
+                sumAllSymbols+=symbol;
+            }
+        }
+        System.out.println(sumAllSymbols);
+    }
+}
