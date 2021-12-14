3f6463eab20349f99ab14d88799ced2f408c6fe4
Radoslav Bekyarov <rbekyarov82@gmail.com>
2021-12-14 13:32:58 +0200

    Streams Files and Directories

---
+++ src/ADV08StreamsFilesAndDirectoriesExercise/p4Count_Character_Types.java
@@ -0,0 +1,44 @@
+package ADV08StreamsFilesAndDirectoriesExercise;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+public class p4Count_Character_Types {
+    public static void main(String[] args) throws IOException {
+        String path = "E:\\folder\\Java-Advanced-Files-and-Streams-Exercises-Resources\\input.txt";
+
+        PrintWriter writer = new PrintWriter("E:\\folder\\Java-Advanced-Files-and-Streams-Exercises-Resources\\output.txt");
+
+        List<String> inputList = Files.readAllLines(Path.of(path));
+        StringBuilder input = new StringBuilder();
+        for (String line : inputList) {
+            input.append(line);
+        }
+        String vowels = "aeiou";
+        String punctuation = "!,.?";
+        String space = " ";
+        int sumVowels = 0;
+        int sumConsonants = 0;
+        int sumPunctuation = 0;
+
+        for (int i = 0; i < input.toString().length(); i++) {
+            String currentSymbol = "" + input.charAt(i);
+            if (vowels.contains(currentSymbol)) {
+                sumVowels++;
+            } else if (punctuation.contains(currentSymbol)) {
+                sumPunctuation++;
+            } else if (space.contains(currentSymbol)) {
+                continue;
+            } else {
+                sumConsonants++;
+            }
+        }
+        writer.println("Vowels: " + sumVowels);
+        writer.println("Consonants: " + sumConsonants);
+        writer.println("Punctuation: " + sumPunctuation);
+        writer.close();
+    }
+}
