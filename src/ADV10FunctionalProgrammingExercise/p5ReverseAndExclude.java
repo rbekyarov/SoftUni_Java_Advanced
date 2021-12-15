7f0e28f30b861d1cd22f746b5d1242ccb0f3df5f
Radoslav Bekyarov <rbekyarov82@gmail.com>
2021-12-15 14:25:54 +0200

    Exercise Functional Programming

---
+++ src/ADV10FunctionalProgrammingExercise/p5ReverseAndExclude.java
@@ -0,0 +1,27 @@
+package ADV10FunctionalProgrammingExercise;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class p5ReverseAndExclude {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<Integer> numbers = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .map(Integer::parseInt).collect(Collectors.toList());
+        Collections.reverse(numbers);
+
+        int n = Integer.parseInt(scanner.nextLine());
+        Predicate<Integer> areDivisible = e -> e % n == 0;
+        numbers.removeIf(areDivisible);
+        Consumer<List<Integer>> printList = list -> list.forEach(e -> System.out.print(e + " "));
+
+        printList.accept(numbers);
+
+    }
+}
