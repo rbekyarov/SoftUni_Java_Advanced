1395fec5c280c4d80be77c874d85cdfd6f0c12fb
Radoslav Bekyarov <rbekyarov82@gmail.com>
2021-12-15 09:27:16 +0200

    Functional Programming

---
+++ src/ADV09FunctionalProgramming/p2SumNumbers.java
@@ -0,0 +1,17 @@
+package ADV09FunctionalProgramming;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class p2SumNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] numbers = Arrays.stream(scanner.nextLine().split(", "))
+                .mapToInt(Integer::parseInt).toArray();
+
+        System.out.println("Count = " + Arrays.stream(numbers).count());
+
+        System.out.println("Sum = " + Arrays.stream(numbers).sum());
+    }
+}
