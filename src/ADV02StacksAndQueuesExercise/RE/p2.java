fe0b8577b5091dd6f14893b6b9c9a313494abf13
Radoslav Bekyarov <rbekyarov82@gmail.com>
2021-12-30 16:49:09 +0200

    StacksAndQueues

---
+++ src/ADV02StacksAndQueuesExercise/RE/p2.java
@@ -0,0 +1,37 @@
+package ADV02StacksAndQueuesExercise.RE;
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class p2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] arr = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+
+        int N = arr[0];
+        int S = arr[1];
+        int X = arr[2];
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(scanner.next());
+            stack.push(num);
+        }
+        for (int i = 0; i < S; i++) {
+            stack.pop();
+        }
+        for (Integer integer : stack) {
+            if (stack.contains(X)) {
+                System.out.println(true);
+                break;
+            } else {
+                if (integer < min) {
+                    min = integer;
+                }
+            }
+        }
+        System.out.println(min);
+    }
+}
