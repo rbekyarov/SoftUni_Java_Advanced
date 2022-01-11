a3c4ae77ec5f6ac4271c211e82b9f9814edc171a
Radoslav Bekyarov <rbekyarov82@gmail.com>
2022-01-11 09:42:20 +0200

    StacksAndQueues

--- src/ADV01StacksAndQueues/RE/p3.java
+++ src/ADV01StacksAndQueues/RE/p3.java
@@ -1,11 +1,23 @@
 package ADV01StacksAndQueues.RE;
 
+import java.util.ArrayDeque;
 import java.util.Scanner;
 
 public class p3 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-
+        int decimal  = Integer.parseInt(scanner.nextLine());
+        ArrayDeque<Integer> binaryStack = new ArrayDeque<>();
+        if (decimal  == 0) {
+            System.out.println("0");
+        } else {
+            while (decimal  > 0) {
+                binaryStack.push(decimal  % 2);
+                decimal  /= 2;
+            }
+            for (Integer binary : binaryStack) {
+                System.out.print(binary);
+            }
+        }
     }
 }
