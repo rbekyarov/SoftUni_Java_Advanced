2c83f41950a518c3174f692186d65b7475438ebb
Radoslav Bekyarov <rbekyarov82@gmail.com>
2022-01-11 09:22:30 +0200

    StacksAndQueues

--- src/ADV01StacksAndQueues/RE/p2.java
+++ src/ADV01StacksAndQueues/RE/p2.java
@@ -1,38 +1,28 @@
 package ADV01StacksAndQueues.RE;
 
-import java.util.ArrayDeque;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class p2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        ArrayDeque<Integer> stack = new ArrayDeque<>();
-        List<String> list = Arrays.stream(scanner.nextLine().split(" ")).collect(Collectors.toList());
-        for (int i = 0; i < list.size(); i++) {
-            if (i == 0) {
-                stack.push(Integer.parseInt(list.get(i)));
-                String sign = list.get(i + 1);
-                if (sign.equals("+")) {
-                    stack.push(stack.pop() + Integer.parseInt(list.get(i + 2)));
-                } else {
-                    stack.push(stack.pop() - Integer.parseInt(list.get(i + 2)));
-                }
-                list.remove(i + 2);
-                list.remove(i + 1);
+        ArrayDeque<String> stack = new ArrayDeque<>();
+        String[] input = scanner.nextLine().split(" ");
+        Collections.addAll(stack, input);
+
+        while (stack.size() > 1) {
+            int firstNum = Integer.parseInt(stack.pop());
+            String singh = stack.pop();
+            int secondNum = Integer.parseInt(stack.pop());
+            int result = 0;
+            if (singh.equals("+")) {
+                result = firstNum + secondNum;
             } else {
-                String sign = list.get(i);
-                if (sign.equals("+")) {
-                    stack.push(stack.pop() + Integer.parseInt(list.get(i + 1)));
-                } else {
-                    stack.push(stack.pop() - Integer.parseInt(list.get(i + 1)));
-                }
-                list.remove(i + 1);
+                result = firstNum - secondNum;
             }
+            stack.push(result+"");
         }
-        System.out.println(stack.pop());
+        System.out.println(stack.peek());
     }
 }
