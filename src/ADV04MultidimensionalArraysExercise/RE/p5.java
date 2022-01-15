97578ef9169c097c236eda6d9968004ca4d1e980
Radoslav Bekyarov <rbekyarov82@gmail.com>
2022-01-15 16:55:00 +0200

    Matrices

--- src/ADV04MultidimensionalArraysExercise/RE/p5.java
+++ src/ADV04MultidimensionalArraysExercise/RE/p5.java
@@ -30,9 +30,9 @@ public class p5 {
     }
 
     private static void printMatrix(int[][] matrix) {
-        for (int i = 0; i < matrix.length ; i++) {
-            for (int j = 0; j <matrix[0].length ; j++) {
-                System.out.print(matrix[i][j]+" ");
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.print(matrix[i][j] + " ");
             }
             System.out.println();
         }
