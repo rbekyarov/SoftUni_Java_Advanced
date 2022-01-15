984c2870536133faf3dbdda97d1e1b7883c43e8e
Radoslav Bekyarov <rbekyarov82@gmail.com>
2022-01-15 11:33:51 +0200

    Matrices

---
+++ src/ADV04MultidimensionalArraysExercise/RE/p2.java
@@ -0,0 +1,26 @@
+package ADV04MultidimensionalArraysExercise.RE;
+
+import java.util.Scanner;
+
+public class p2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int rows = scanner.nextInt();
+        int cols = scanner.nextInt();
+        String [][] matrix = new String[rows][cows];
+        char startletter = 'a';
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                String palindrome = "" + startletter + (char) (startletter + col) + startletter;
+                matrix[row][col] = palindrome;
+            }
+            startletter = (char) (startletter + 1);
+        }
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+
+    }
+}
