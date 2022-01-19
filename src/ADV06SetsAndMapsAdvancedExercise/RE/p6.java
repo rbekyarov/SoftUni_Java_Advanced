427de5a4862b16149e6f0912d30a7d96605792f8
Radoslav Bekyarov <rbekyarov82@gmail.com>
2022-01-19 13:51:32 +0200

    SETS AND MAPS

--- src/ADV06SetsAndMapsAdvancedExercise/RE/p6.java
+++ src/ADV06SetsAndMapsAdvancedExercise/RE/p6.java
@@ -8,29 +8,29 @@ public class p6 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
-        Map<String,Integer> map = new LinkedHashMap<>();
-        int count =0;
+        Map<String, Integer> map = new LinkedHashMap<>();
+        int count = 0;
         String name = "";
 
         while (!input.equals("stop")) {
-            int quantity =0;
-            if (count%2==0){
-               name = input;
+            int quantity = 0;
+            if (count % 2 == 0) {
+                name = input;
             } else {
-                 quantity = Integer.parseInt(input);
+                quantity = Integer.parseInt(input);
             }
-            if(!map.containsKey(name)){
-                map.put(name,quantity);
-            }else {
-                int currentQuantity =map.get(name);
-                map.put(name,quantity+currentQuantity);
+            if (!map.containsKey(name)) {
+                map.put(name, quantity);
+            } else {
+                int currentQuantity = map.get(name);
+                map.put(name, quantity + currentQuantity);
             }
             count++;
             input = scanner.nextLine();
 
         }
         for (Map.Entry<String, Integer> entry : map.entrySet()) {
-            System.out.println(entry.getKey()+" -> "+entry.getValue());
+            System.out.println(entry.getKey() + " -> " + entry.getValue());
         }
     }
 }
