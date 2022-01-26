b2a1e77feebb150cfd17b88f8ac911d1e661f7d7
Radoslav Bekyarov <rbekyarov82@gmail.com>
2022-01-26 10:57:03 +0200

    Exam Prep

--- src/ADV17ExamPrep/Exam15December2021/p2.java
+++ src/ADV17ExamPrep/Exam15December2021/p2.java
@@ -1,40 +1,116 @@
 package ADV17ExamPrep.Exam15December2021;
 
-import java.util.Arrays;
+
+
 import java.util.Scanner;
 
 public class p2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         int energy = Integer.parseInt(scanner.nextLine());
-        int size = Integer.parseInt(scanner.nextLine());
+        int rows = Integer.parseInt(scanner.nextLine());
+        char [][] field = new char[rows][rows];
 
-        String [][] fieldSparta = new String[size][size];
-
-        for (int row = 0; row <size ; row++) {
-            String[]data = scanner.nextLine().split("");
-            fieldSparta[row]=data;
+        //въвеждаме полето
+        for (int row = 0; row < rows; row++) {
+            field[row] = scanner.nextLine().toCharArray(); //"--H--".toCharArray() -> ['-', '-', 'H', '-', '-']
         }
-        int[]targetIndexes = new int[2];
-        int[]startIndexes = new int[2];
-        for (int i = 0; i <size ; i++) {
-            for (int j = 0; j <size ; j++) {
 
-                if(fieldSparta[i][j].equals("H")){
-                    targetIndexes[0]=i;
-                    targetIndexes[1]=j;
-
-                }
-                else if(fieldSparta[i][j].equals("P")){
-                     startIndexes[0] = i;
-                     startIndexes[1] = j;
+        //намираме къде е Парис първоначално
+        int parisRow = 0;
+        int parisCol = 0;
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < field[row].length; col++) {
+                if (field[row][col] == 'P') {
+                    parisRow = row;
+                    parisCol = col;
                 }
             }
         }
-        while (energy!=0){
 
+        while (true) {
+            String command = scanner.nextLine();
+            //"{direction} {row have spartan} {col have spartan}".split(" ") -> ["dir", "row", "col"]
+            String direction = command.split(" ")[0]; //"up", "down", "left", "right"
+            int enemyRow = Integer.parseInt(command.split(" ")[1]);
+            int enemyCol = Integer.parseInt(command.split(" ")[2]);
+
+            field[parisRow][parisCol] = '-';
+            field[enemyRow][enemyCol] = 'S';
+            //преместване
+            switch (direction) {
+                case "up":
+                    if (parisRow - 1 >= 0) {
+                        parisRow--;
+                    }
+                    break;
+                case "down":
+                    if (parisRow + 1 < field.length) {
+                        parisRow++;
+                    }
+                    break;
+                case "left":
+                    if (parisCol - 1 >= 0) {
+                        parisCol--;
+                    }
+                    break;
+                case "right":
+                    if(parisCol + 1 < field.length) {
+                        parisCol++;
+                    }
+
+                    break;
+            }
+            //след преместване
+            //1. намаляме енергията с 1
+            energy--;
+            //2. проверка дали е умрял
+            if (energy <= 0) {
+                parisDead(field, parisRow, parisCol);
+                return;
+            }
+
+            //3. има ли спартанец там където се е преместил
+            if (field[parisRow][parisCol] == 'S') {
+                //3.1. победа над спартанеца -> намаляме енергията с 2
+                energy -= 2;
+                //3.2 проверка дали е умрял -> ако енергията <= 0
+                if (energy <= 0) {
+                    parisDead(field, parisRow, parisCol);
+                    return;
+                } else {
+                    field[parisRow][parisCol] = '-';
+                }
+            }
+            //4. има ли Елена там където се е преместил
+            else if (field[parisRow][parisCol] == 'H') {
+                //4.1. на мястото, където е Елена -> слагаме -
+                field[parisRow][parisCol] = '-';
+                //4.2. отпечатване -> останала енергия
+                System.out.printf("Paris has successfully abducted Helen! Energy left: %d%n", energy);
+                //4.3. отпечатваме матрицата
+                printField(field);
+                return;
+            }
         }
+    }
 
+    public static void parisDead(char[][] field, int parisRow, int parisCol) {
+        //Парис умира
+        //1. там където е бил слагаме X
+        field[parisRow][parisCol] = 'X';
+        //2. отпечатване -> къде е умрял
+        System.out.printf("Paris died at %d;%d.%n", parisRow, parisCol);
+        //3. отпечатваме матрицата
+        printField(field);
+    }
+
+    private static void printField(char[][] field) {
+        for (int row = 0; row < field.length; row++) {
+            for (int col = 0; col < field[row].length; col++) {
+                System.out.print(field[row][col]);
+            }
+            System.out.println();
+        }
     }
 }
