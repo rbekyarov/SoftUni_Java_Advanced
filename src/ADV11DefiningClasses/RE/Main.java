bab37981868acbfb08401bcc2104197f0517c46d
Radoslav Bekyarov <rbekyarov82@gmail.com>
2022-01-29 14:34:06 +0200

    Defining Classes

--- src/ADV11DefiningClasses/RE/Main.java
+++ src/ADV11DefiningClasses/RE/Main.java
@@ -11,16 +11,12 @@ public class Main {
         String input = scanner.nextLine();
         Map<Integer, BankAccount> mapAccounts = new HashMap<>();
         while (!input.equals("end")) {
-
-
             if (input.split("\\s+")[0].equals("Create")) {
                 bankAccount = new BankAccount();
                 if (!mapAccounts.containsKey(bankAccount.getId())) {
                     mapAccounts.put(bankAccount.getId(), bankAccount);
                     System.out.printf("Account ID%d created%n", bankAccount.getId());
                 }
-
-
             } else if (input.split("\\s+")[0].equals("Deposit")) {
                 int id = Integer.parseInt(input.split("\\s+")[1]);
                 double amount = Double.parseDouble(input.split("\\s+")[2]);
@@ -32,13 +28,9 @@ public class Main {
                 } else {
                     System.out.println("Account does not exist");
                 }
-
-
             } else if (input.split("\\s+")[0].equals("SetInterest")) {
                 double newInterestRate = Double.parseDouble(input.split("\\s+")[1]);
                 BankAccount.setInterestRate(newInterestRate);
-
-
             } else if (input.split("\\s+")[0].equals("GetInterest")) {
                 int id = Integer.parseInt(input.split("\\s+")[1]);
                 int years = Integer.parseInt(input.split("\\s+")[2]);
@@ -50,9 +42,7 @@ public class Main {
                 } else {
                     System.out.println("Account does not exist");
                 }
-
             }
-
             input = scanner.nextLine();
         }
     }
