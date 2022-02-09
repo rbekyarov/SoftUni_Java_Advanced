fde43b22f50e27bfed6e90951580f71f8b8231ba
Radoslav Bekyarov <rbekyarov82@gmail.com>
2022-02-09 15:12:57 +0200

    EXAM PREP

--- src/ADV17ExamPrep/E26June2021/p2.java
+++ src/ADV17ExamPrep/E26June2021/p2.java
@@ -3,128 +3,139 @@ package ADV17ExamPrep.E26June2021;
 import java.util.Scanner;
 
 public class p2 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
-        int rows = Integer.parseInt(scanner.nextLine());
-        char[][] field = new char[rows][rows];
-        String[] commands = scanner.nextLine().split(", ");
-        for (int row = 0; row < rows; row++) {
-            String line = scanner.nextLine().replaceAll("\\s+", "");
-            field[row] = line.toCharArray();
-        }
-        int pythonLength = 1;
-        int pythonRow = 0;
-        int pythonCol = 0;
-        for (int row = 0; row < rows; row++) {
-            for (int col = 0; col < field[row].length; col++) {
-                if (field[row][col] == 's') {
-                    pythonRow = row;
-                    pythonCol = col;
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+
+            int rows = Integer.parseInt(scanner.nextLine());
+            char[][] field = new char[rows][rows];
+
+            String[] commands = scanner.nextLine().split(", ");
+            for (int row = 0; row < rows; row++) {
+                String line = scanner.nextLine().replaceAll("\\s+", "");
+                field[row] = line.toCharArray();
+            }
+
+            int pythonLength = 1;
+            int pythonRow = 0;
+            int pythonCol = 0;
+
+            boolean found = false;
+            for (int row = 0; row < rows; row++) {
+                for (int col = 0; col < field[row].length; col++) {
+                    if (field[row][col] == 's') {
+                        pythonRow = row;
+                        pythonCol = col;
+                        found = true;
+                    }
+                }
+                if (found) {
+                    break;
                 }
             }
-        }
-        boolean isEaten = false;
-        for (int i = 0; i < commands.length; i++) {
-            String command = commands[i];
-            switch (command) {
-                case "up":
-                    if (pythonRow - 1 >= 0) {
-                        field[pythonRow][pythonCol] = '*';
-                        pythonRow--;
 
-                    } else {
-                        field[pythonRow][pythonCol] = '*';
-                        pythonRow = field.length - 1;
-                        field[pythonRow][pythonCol] = 's';
-                    }
-                    break;
-                case "down":
-                    if (pythonRow + 1 < field.length) {
-                        field[pythonRow][pythonCol] = '*';
-                        pythonRow++;
+            boolean isEaten = false;
+            for (int i = 0; i < commands.length; i++) {
 
-                    } else {
-                        field[pythonRow][pythonCol] = '*';
-                        pythonRow = 0;
-                        field[pythonRow][pythonCol] = 's';
-                    }
-                    break;
-                case "left":
-                    if (pythonCol - 1 >= 0) {
-                        field[pythonRow][pythonCol] = '*';
-                        pythonCol--;
-
-                    } else {
-                        field[pythonRow][pythonCol] = '*';
-                        pythonCol = field.length - 1;
-                        ;
-                        field[pythonRow][pythonCol] = 's';
-                    }
-                    break;
-                case "right":
-                    if (pythonCol + 1 < field.length) {
-                        field[pythonRow][pythonCol] = '*';
-                        pythonCol++;
-
-                    } else {
-                        field[pythonRow][pythonCol] = '*';
-                        pythonCol = 0;
-                        field[pythonRow][pythonCol] = 's';
-                    }
-                    break;
-            }
-            if (field[pythonRow][pythonCol] == 'f') {
-                pythonLength++;
+                String command = commands[i];
                 field[pythonRow][pythonCol] = '*';
+
+                switch (command) {
+                    case "up":
+                        if (pythonRow - 1 >= 0) {
+                            pythonRow--;
+
+                        } else {
+                            pythonRow = field.length - 1;
+                        }
+                        break;
+                    case "down":
+                        if (pythonRow + 1 < field.length) {
+                            pythonRow++;
+
+                        } else {
+                            pythonRow = 0;
+                        }
+                        break;
+                    case "left":
+                        if (pythonCol - 1 >= 0) {
+                            pythonCol--;
+
+                        } else {
+                            pythonCol = field.length - 1;
+                        }
+                        break;
+                    case "right":
+                        if (pythonCol + 1 < field.length) {
+                            pythonCol++;
+
+                        } else {
+                            pythonCol = 0;
+                        }
+                        break;
+                }
+                if (field[pythonRow][pythonCol] == 'f') {
+                    pythonLength++;
+
+                    field[pythonRow][pythonCol] = '*';
+                    if (!chekMoreFood(field)) {
+
+                        isEaten = true;
+                        break;
+                    }
+                    field[pythonRow][pythonCol] = 's';
+
+                } else if (field[pythonRow][pythonCol] == 'e') {
+                    System.out.println("You lose! Killed by an enemy!");
+                    return;
+
+                } else {
+                    field[pythonRow][pythonCol] = 's';
+                }
+
                 if (!chekMoreFood(field)) {
 
                     isEaten = true;
                     break;
                 }
-                field[pythonRow][pythonCol] = 's';
-            } else if (field[pythonRow][pythonCol] == 'e') {
-                System.out.println("You lose! Killed by an enemy!");
-                return;
+            }
+
+            if (isEaten) {
+                System.out.println("You win! Final python length is " + pythonLength);
             } else {
-                field[pythonRow][pythonCol] = 's';
+                System.out.printf("You lose! There is still %d food to be eaten.%n", leftFood(field));
             }
         }
-        if (isEaten) {
-            System.out.println("You win! Final python length is " + pythonLength);
-        } else {
-            System.out.printf("You lose! There is still %d food to be eaten.%n", leftFood(field));
-        }
-    }
 
-    private static int leftFood(char[][] field) {
-        int left = 0;
-        for (int row = 0; row < field.length; row++) {
-            for (int col = 0; col < field[row].length; col++) {
-                if (field[row][col] == 'f') {
-                    left++;
+        private static int leftFood(char[][] field) {
+            int left = 0;
+            for (int row = 0; row < field.length; row++) {
+                for (int col = 0; col < field[row].length; col++) {
+                    if (field[row][col] == 'f') {
+                        left++;
+                    }
                 }
             }
+            return left;
         }
-        return left;
-    }
-    private static boolean chekMoreFood(char[][] field) {
-        boolean isFind = false;
-        for (int row = 0; row < field.length; row++) {
-            for (int col = 0; col < field[row].length; col++) {
-                if (field[row][col] == 'f') {
-                    isFind = true;
+
+        private static boolean chekMoreFood(char[][] field) {
+            boolean isFind = false;
+            for (int row = 0; row < field.length; row++) {
+                for (int col = 0; col < field[row].length; col++) {
+                    if (field[row][col] == 'f') {
+                        isFind = true;
+                        break;
+                    }
+                }
+                if (isFind) {
                     break;
                 }
             }
             if (isFind) {
-                break;
+                return true;
+            } else {
+                return false;
             }
         }
-        if (isFind) {
-            return true;
-        } else {
-            return false;
-        }
     }
-}
