ebded9f4c7c16b5533809dac7c2f53654e4fbc35
Radoslav Bekyarov <rbekyarov82@gmail.com>
2022-02-17 16:13:50 +0200

    Iterator Comparator

---
+++ src/ADV16IteratorsAndComparatorsExercise/09. Linked List Traversal/src/collections/Node.java
@@ -0,0 +1,11 @@
+public class Node {
+    Node prev;
+    Node next;
+    Integer value;
+
+    public Node(int value) {
+        this.next = null;
+        this.prev = null;
+        this.value = value;
+    }
+}
